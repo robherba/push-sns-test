7863d6c749032ff8a81d9ef6281c187c2f6fec12
Roberto Hernandez <roberthb94@gmail.com>
2019-04-07 21:46:06 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import PushNotification from 'react-native-push-notification';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+    
+    componentDidMount() {
+        console.log('Did mount')
+        PushNotification.configure({
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function (token) {
+            console.log('TOKEN:', token);
+            },
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: function (notification) {
+            console.log('NOTIFICATION:', notification);
+            },
+            // ANDROID ONLY: GCM Sender ID (optional — not required for local notifications, but is need to receive remote push notifications)
+            senderID: "170550070607",
+            // IOS ONLY (optional): default: all — Permissions to register.
+            permissions: {
+            alert: true,
+            badge: true,
+            sound: true
+            },
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+            /**
+             * (optional) default: true
+             * — Specified if permissions (ios) and token (android and ios) will requested or not,
+             * — if not, you must call PushNotificationsHandler.requestPermissions() later
+             */
+            requestPermissions: true,
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text style={styles.welcome}>Welcome to React Native!</Text>
+              <Text style={styles.instructions}>To get started, edit App.js</Text>
+              <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
